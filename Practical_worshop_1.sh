8a187f47b5c00e2b9b2cc6f1a0a03029b971cc1b
sammy1208-g <samchamorroca@uide.edu.ec>
2026-03-18 14:02:07 +0000

    my keys

--- Practical_worshop_1.sh
+++ Practical_worshop_1.sh
@@ -2,3 +2,7 @@ uname -a
 which gpg
 gpg --version
 gpg --full-generate-key
+gpg --list-keys
+gpg --armor --export chamorrosamy525@gmail.com > mi_llave publica.asc
+ls mi_llave_publica.asc
+gpg --armor --export
