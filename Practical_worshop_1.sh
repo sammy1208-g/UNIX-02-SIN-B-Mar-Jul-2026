3b9fdd561215ba3c099cbc9985b83ddb3686aa84
sammy1208-g <samchamorroca@uide.edu.ec>
2026-03-23 12:53:55 +0000

    codespaces

--- Practical_worshop_1.sh
+++ Practical_worshop_1.sh
@@ -1,8 +1,11 @@
 uname -a
 which gpg
 gpg --version
-gpg --full-generate-key
+gpg --full-generate-key  # generar clave
 gpg --list-keys
 gpg --armor --export chamorrosamy525@gmail.com > mi_llave publica.asc
 ls mi_llave_publica.asc
 gpg --armor --export
+gpg --list-secret-keys --keyid-format=long #para listar las llaves privadas
+# 23909E79DC4B0DF0
+gpg --armor --export-secret-keys # para especificar el hash de las llaves
