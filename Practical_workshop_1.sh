99213d377a5f7158ed5ed73f3058c4755a146ece
sammy1208-g <samchamorroca@uide.edu.ec>
2026-03-25 12:50:17 +0000

    Generate file in C language: "greeting"

--- Practical_workshop_1.sh
+++ Practical_workshop_1.sh
@@ -1,7 +1,7 @@
 uname -a # Da la información completa del sistema operativo  / respuesta: Linux codespaces-dd4d4b 6.8.0-1044-azure #50~22.04.1-Ubuntu SMP Wed Dec  3 15:13:22 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux
 which gpg # Nos dice en qué ruta está instalado GPG / respuesta: /usr/bin/gpg
 gpg --version # # Muestra la versión de GPG instalada / respuesta : gpg (GnuPG) 2.4.4 ....
-gpg --full-generate-key # generar clave
+gpg --full-generate-key # generate key
 gpg --list-keys # listar llaves 
 gpg --armor --export chamorrosamy525@gmail.com > mi_llave publica.asc # exporta la clave pública y la guarda en un archivo .asc
 ls mi_llave_publica.asc # verifica que el archivo se haya creado
@@ -9,6 +9,7 @@ gpg --armor --export # exporta clave pública
 gpg --list-secret-keys --keyid-format=long # para listar las llaves privadas
 gpg --armor --export-secret-keys  # para especificar el hash de las llaves
 # gpg --pinentry-mode loopback --armor --export-secret-keys C942833FAF2766C1  / EN MICASO NO ME VALIO  EL ANTERIOR COMANDO Y APLIQUE ESTE COMANDO PARA QUE FUNIONARA Y ME SALIERA MI LLAVE PRIVADA.
+# El comando permite exportar la clave privada en formato ASCII, utilizando el modo loopback para ingresar la contraseña directamente en la terminal, lo cual es útil en entornos sin interfaz gráfica.
 gpg --import JenniMICOMPA_llave_publica.asc  #  compartir llave
 gpg --list-keys # listar llaves de mi compa
 $ echo "este mensaje es secreto" > doc_no_cifrado.txt # guardar un mensaje para nuestro compañero, en este caso "este mensaje es secreto"
