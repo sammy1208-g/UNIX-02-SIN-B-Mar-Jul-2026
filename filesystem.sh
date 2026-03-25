9693908d0719c10ef4e36462146793252da60c11
sammy1208-g <samchamorroca@uide.edu.ec>
2026-03-25 13:19:09 +0000

    commands 2

--- filesystem.sh
+++ filesystem.sh
@@ -1,3 +1,6 @@
 # Comandos - Commands
 pwd # Print word directory / imprime la ruta en la que estoy en ese momento. - Prints the current path.
+cd / # change directory  - lleva a la carpeta principal (raíz) del sistema
+ls -F # muestra los archivos indicando su tipo /  Displays files indicating their type.
+ls -i # muestra los archivos junto con su identificador interno del sistema (inodo) / It displays the files along with their internal system identifier (inode).
 
