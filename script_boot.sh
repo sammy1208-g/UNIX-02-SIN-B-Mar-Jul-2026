b2b299e92f4888067b03199430f608674e1e1d62
sammy1208-g <samchamorroca@uide.edu.ec>
2026-04-06 12:33:05 +0000

    cd_commands

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+cd # change directory / It takes you directly to your home directory.
+cd /home/codespace #  It moves you to a specific path.
+cd ~ # It is a shortcut for your home directory.
+cd $HOME # It is an environment variable that stores your home directory path.
