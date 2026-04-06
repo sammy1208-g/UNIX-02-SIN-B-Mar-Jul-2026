d17f4d89d6171c4241b0aae1f76c1999d077ac45
sammy1208-g <samchamorroca@uide.edu.ec>
2026-04-06 12:59:07 +0000

    commands_man_and_creates_prueba

--- script_boot.sh
+++ script_boot.sh
@@ -19,3 +19,15 @@ stat .  # Shows detailed information about the current directory (.)
 # Change: 2026-04-06 12:35:44.541507445 +0000
 #Birth: 2026-04-06 12:35:44.541507445 +0000
 # Device : 0,45 / Identifier of the storage device
+mkdir -p /tmp/prueba/sub1 /tmp/prueba/sub2 # Creates directories recursively (including parent folders if they don’t exist).
+stat /tmp/prueba # Shows detailed information about the prueba directory
+# Results:
+#  Size: 4096            Blocks: 8          IO Block: 4096   directory
+#Device: 8,1     Inode: 1179663     Links: 4
+#Access: (0756/drwxr-xrw-)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+#Access: 2026-04-06 12:52:52.526521831 +0000
+#Modify: 2026-04-06 12:52:52.527521831 +0000
+#Change: 2026-04-06 12:52:52.527521831 +0000
+#Birth: 2026-04-06 12:52:52.526521831 +0000
+#Device:  8,1 / Storage device ID
+man mkdir -p /tmp/prueba/sub1 /tmp/prueba/sub2 # This open the manual 
