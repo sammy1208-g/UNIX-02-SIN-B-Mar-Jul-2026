2d7c7b5d14dd519529cba24a18d6bf5bd2437cf5
sammy1208-g <samchamorroca@uide.edu.ec>
2026-04-06 12:39:26 +0000

    commands_Create_a_new_directory

--- script_boot.sh
+++ script_boot.sh
@@ -1,4 +1,11 @@
 cd # change directory / It takes you directly to your home directory.
 cd /home/codespace #  It moves you to a specific path.
 cd ~ # It is a shortcut for your home directory.
-cd $HOME # It is an environment variable that stores your home directory path.
+cd $HOME # It is an environment variable that stores your home directory path
+mkdir proyecto # Creates a new directory (folder) called proyecto.
+cd proyecto/ # Moves into the proyecto directory
+ls -lai # Lists files  
+#Results : 
+#total 12
+# 925550 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:35 .
+# 918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:35 ..
