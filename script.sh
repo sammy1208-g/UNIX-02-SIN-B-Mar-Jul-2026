24a9736be16bde48d326d89adbc597a3425026de
sammy1208-g <samchamorroca@uide.edu.ec>
2026-04-13 12:35:26 +0000

    sintaxis_commit

---
+++ script.sh
@@ -0,0 +1,7 @@
+ls -a # Short option
+ls --all # Long option
+ls -a / 
+# Command, option, arguments
+# Command (ls) Lists the contents of a directory.
+#Option (-a) Shows all files, including hidden files.
+#Arguments (/) Refers to the root directory.
