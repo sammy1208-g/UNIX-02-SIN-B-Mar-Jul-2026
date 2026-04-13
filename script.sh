557090532527d8ae220298e5383e47357ea5765e
sammy1208-g <samchamorroca@uide.edu.ec>
2026-04-13 12:46:10 +0000

    commit_ls

--- script.sh
+++ script.sh
@@ -5,3 +5,13 @@ ls -a /
 # Command (ls) Lists the contents of a directory.
 #Option (-a) Shows all files, including hidden files.
 #Arguments (/) Refers to the root directory.
+
+ls -l -a -h # Long option
+# ls : Lists files
+# -l : Uses long format (permissions, size, date, etc.)
+#-a : Shows all files, including hidden files
+#-h : Shows sizes in human-readable format (KB, MB)
+ls -l -ah # long option
+ls -lah # Short option
+
+
