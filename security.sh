e926f9a217187031149afbdb31622e00a49fa64f
sammy1208-g <samchamorroca@uide.edu.ec>
2026-05-04 13:46:45 +0000

    commit-cat with /etc/group and /etc/gshadow

--- security.sh
+++ security.sh
@@ -12,3 +12,41 @@ groups
 groups $USER 
 # It displays the groups, but specifies the user using a variable; the $ symbol indicates that a system variable is being used. 
 # `$USER` contains the current user's name.
+
+id -u # Displays only the current user's UID
+id -g # Displays the user's primary GID.
+id -G # It displays all the GIDs of the groups to which the user belongs.
+
+cat /etc/group | grep codespace # Displaying only the lines in the groups file that contain the word "codespace" will result in an error because that directory is not present.
+#Results: cat: etc/group: No existe el fichero o el directorio
+
+cat /etc/group # Displaying the entire contents of the system's groups file, that is, showing the complete list of existing groups.
+#Results 
+# src:x:40:
+# shadow:x:42:
+# utmp:x:43:
+# video:x:44:
+# sasl:x:45:
+# plugdev:x:46:
+# staff:x:50:
+# games:x:60:
+# users:x:100:
+#.....
+# Choose one
+
+cat /etc/group | grep users # Displaying only the system groups that contain the word "user".
+#Results:  users:x:100:
+
+cat /etc/gshadow # Display protected information about system groups.
+#Results:
+# root:*::
+#daemon:*::
+#bin:*::
+#sys:*::
+#adm:*::
+#tty:*::
+#disk:*::
+#lp:*::
+#ews:*::
+# ....
+
