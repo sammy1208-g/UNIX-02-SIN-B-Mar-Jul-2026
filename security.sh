8bbb384a38e15b115ee257eb2d6970c67fa9cd83
sammy1208-g <samchamorroca@uide.edu.ec>
2026-05-04 12:49:56 +0000

    command-user id and cat

---
+++ security.sh
@@ -0,0 +1,7 @@
+# Commands
+
+id 
+# It displays information about the user who is currently using the system.
+
+cat /etc/passwd | head -10
+# This command displays only the first 10 lines of the system's /etc/passwd file for system users.
