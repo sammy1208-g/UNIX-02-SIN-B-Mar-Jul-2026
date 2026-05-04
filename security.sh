dac93a828add56a622ce8ebf5d30989a608dd355
sammy1208-g <samchamorroca@uide.edu.ec>
2026-05-04 13:28:18 +0000

    Commit-groups

--- security.sh
+++ security.sh
@@ -5,3 +5,10 @@ id
 
 cat /etc/passwd | head -10
 # This command displays only the first 10 lines of the system's /etc/passwd file for system users.
+
+groups
+# The `groups` command (`groups`) displays the groups to which the current user belongs.
+
+groups $USER 
+# It displays the groups, but specifies the user using a variable; the $ symbol indicates that a system variable is being used. 
+# `$USER` contains the current user's name.
