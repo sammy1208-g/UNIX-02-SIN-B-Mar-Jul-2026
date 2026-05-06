7dedb9e88052e7d77a7e3444397b6a9877dd7e9e
sammy1208-g <samchamorroca@uide.edu.ec>
2026-05-06 13:34:10 +0000

    commit-mkdir -p / prepare practice scenario

--- security.sh
+++ security.sh
@@ -128,3 +128,13 @@ id root #Lost all other groups
 #Restore
  sudo usermod -aG diseno,marketing,grupo_temporal root
  id root # Restored
+
+# Preparate the practice scenario
+# * -p ( parent)
+mkdir -p ~/lab_chgrp/{proyectos,reportes,scripts}
+touch ~/lab_chgrp/proyectos/app.py
+touch ~/lab_chgrp/proyectos/config.json
+touch ~/lab_chgrp/reportes/informe.txt
+touch ~/lab_chgrp/scripts/deploy.sh
+
+
