79f6c65de4a4908085aecfcc44d3700c1378a161
sammy1208-g <samchamorroca@uide.edu.ec>
2026-05-27 13:34:21 +0000

    commit- file inside subshell

--- security2.sh
+++ security2.sh
@@ -31,3 +31,36 @@ newgrp desarrolladores
 #Verify that the active group has changed
 id -gn
 echo "Nuevo grupo activo: $(id -gn)"
+#Results 
+# Nuevo grupo activo: desarrolladores
+
+#Create a file inside the subshell.
+touch ~/dentro_de_newgrp.txt
+ls -la ~/dentro_de_newgrp.txt
+# Results:
+# -rw-r--r-- 1 root desarrolladores 0 may 27 13:29 /root/dentro_de_newgrp.txt
+
+#The group is now developers
+# Create a directory
+mkdir -p ~/proyecto_dev/src
+ls -la ~/
+# Results:
+#total 68
+#drwx------ 1 root root             4096 may 27 13:30 .
+#drwxr-xr-x 1 root root             4096 may 27 12:46 ..
+#-rw-r--r-- 1 root root                0 may 27 13:06 antes_de_newgrp.txt
+#-rw-r--r-- 1 root root             5578 may 24 04:01 .bashrc
+#-rw-r--r-- 1 root root              607 may 24 04:01 .bashrc.original
+#drwxr-xr-x 3 root root             4096 may 27 12:46 .cache
+#drwx------ 3 root root             4096 may 27 12:46 .config
+#-rw-r--r-- 1 root desarrolladores     0 may 27 13:29 dentro_de_newgrp.txt
+#lrwxrwxrwx 1 root root               47 may 27 12:46 .docker -> /workspaces/.codespaces/.persistedshare/.docker
+#drwxr-xr-x 3 root root             4096 may 27 12:46 .dotnet
+#drwx------ 4 root root             4096 may 27 12:46 .local
+#-rw-r--r-- 1 root root              132 may 15 11:37 .profile
+#drwxr-xr-x 3 root desarrolladores  4096 may 27 13:30 proyecto_dev
+#drwx------ 2 root root             4096 may 27 12:45 .ssh
+#-rw-r--r-- 1 root root                0 may 27 12:56 test_grupo_heredado.txt
+#drwxr-xr-x 5 root root             4096 may 27 12:46 .vscode-remote
+#-rw------- 1 root root              519 may 27 13:13 .zsh_history
+#-rw-r--r-- 1 root root            10976 may 27 12:46 .zshrc
