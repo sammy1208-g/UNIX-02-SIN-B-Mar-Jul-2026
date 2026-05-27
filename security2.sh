b364011d9e2648663e3ff87eb2789a222773b6fa
sammy1208-g <samchamorroca@uide.edu.ec>
2026-05-27 13:07:33 +0000

    commit- before newgrp

--- security2.sh
+++ security2.sh
@@ -7,3 +7,13 @@ ls -la ~/test_grupo_heredado.txt
 # Results
 # -rw-r--r-- 1 root root 0 may 27 12:56 /root/test_grupo_heredado.txt
 # The group is the main group of user.
+
+# See the current parent group
+id -gn
+echo "Grupo actual: $(id -gn)"
+
+#Create file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
+#Results
+# -rw-r--r-- 1 root root 0 may 27 13:06 /root/antes_de_newgrp.txt
