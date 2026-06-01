592899ae29c1b50f66e0ebb41ffce622ca1146f5
sammy1208-g <samchamorroca@uide.edu.ec>
2026-06-01 13:17:59 +0000

    commit-x , +x

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 #!/bin/bash -x  # Muestra los comandos en tiempo real- Display commands in real time.
+set -x # Enables trace mode. 
 
 bash --version
 env
@@ -13,3 +14,5 @@ df --human-readable
 #bash -r blackhatbash1.sh # -r Para ejecutar modo restringido. For running restricted mode
 #bash -n blackhatbash1.sh # -n muestra errores de sintaxis. Displays syntax errors, Debugging.
 # bash -x blackhatbash1.sh # -x Activar modo verboso. Enable verbose mode.
+
+set +x # Disables trace mode.
