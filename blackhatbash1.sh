be045f3de164b357a51734aaa56a780c54a9788e
sammy1208-g <samchamorroca@uide.edu.ec>
2026-06-01 13:11:41 +0000

    commit-bash -r,-n,-x

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -10,4 +10,6 @@ echo ${OSTYPE}
 ps -e -f
 ps -ef
 df --human-readable
-bash -r blackhatbash1.sh # -r Para ejecutar modo restringido. for running restricted mode
+#bash -r blackhatbash1.sh # -r Para ejecutar modo restringido. For running restricted mode
+#bash -n blackhatbash1.sh # -n muestra errores de sintaxis. Displays syntax errors, Debugging.
+# bash -x blackhatbash1.sh # -x Activar modo verboso. Enable verbose mode.
