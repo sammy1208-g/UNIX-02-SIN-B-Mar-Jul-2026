e402b53fff044aaf7d7d5cd3b5d5700b0f1a3b50
sammy1208-g <samchamorroca@uide.edu.ec>
2026-06-03 13:32:56 +0000

    commit- command operators

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -4,3 +4,8 @@ touch test && touch test123
 (ls; ps)
 ls ; ps; whoami
 lzl || echo "the lzl command failed" 
+
+ls -l / &> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
