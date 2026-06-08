e508d3f24eba68923af8639c5021d34b0ad35b64
sammy1208-g <samchamorroca@uide.edu.ec>
2026-06-08 13:24:46 +0000

    commit- exit

--- exit_codes.sh
+++ exit_codes.sh
@@ -8,3 +8,7 @@ echo "The exit code of the ls command was: $?"
 lzl 2> /dev/null
 echo "The exit code of the non-existing lzl command was: $?"
 
+# Sets the exit code of the script to be 223
+
+echo "Exiting with exit code: 223"
+exit 223
