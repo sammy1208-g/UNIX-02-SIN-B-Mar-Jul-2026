467274e374217784043eb6511cfd3edd86dd6144
sammy1208-g <samchamorroca@uide.edu.ec>
2026-06-08 12:59:32 +0000

    commit- change $@ to $*

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -9,8 +9,14 @@ echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
 ping -c 3 "${TARGET}"
 
-chmod u+x ping_with_arguments.sh
-./ping_with_arguments.sh nostarch.com
+#chmod u+x ping_with_arguments.sh
+#./ping_with_arguments.sh nostarch.com
 
 echo "The arguments are: $@"
 echo "The total number of arguments is: $#"
+
+# Change "$@" to "$*" to observe behavior.
+for args in "$@"; do
+ echo "${args}"
+done
+
