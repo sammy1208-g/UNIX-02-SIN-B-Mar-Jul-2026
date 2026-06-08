b36fcafa0e1e5f55a33359287833c3a6be42e37c
sammy1208-g <samchamorroca@uide.edu.ec>
2026-06-08 13:16:46 +0000

    command - exit codes

---
+++ exit_codes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Experimenting with exit codes
+
+ls -l > /dev/null
+echo "The exit code of the ls command was: $?"
+
+lzl 2> /dev/null
+echo "The exit code of the non-existing lzl command was: $?"
+
