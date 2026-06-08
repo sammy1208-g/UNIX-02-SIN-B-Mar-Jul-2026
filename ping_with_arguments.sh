286eb361ae693da01232a64ba9dde8bdc6044dda
sammy1208-g <samchamorroca@uide.edu.ec>
2026-06-08 12:52:43 +0000

    commit- ping

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -7,4 +7,10 @@ TARGET="${1}"
 
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-ping "${TARGET}"
+ping -c 3 "${TARGET}"
+
+chmod u+x ping_with_arguments.sh
+./ping_with_arguments.sh nostarch.com
+
+echo "The arguments are: $@"
+echo "The total number of arguments is: $#"
