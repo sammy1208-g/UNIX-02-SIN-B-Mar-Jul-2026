6f247b3b9544986364b243beea835817b767cc24
sammy1208-g <samchamorroca@uide.edu.ec>
2026-06-10 12:55:14 +0000

    commit- file test1234

--- linking_conditions.sh
+++ linking_conditions.sh
@@ -12,3 +12,8 @@ mkdir "${DIR_NAME}"
 if [[ -f "${DIR_NAME}" ]] || [[ -d "${DIR_NAME}" ]]; then
  echo "${DIR_NAME} is either a file or a directory."
 fi
+
+
+ if touch test1234; then
+ echo "OK: file created"
+ fi
