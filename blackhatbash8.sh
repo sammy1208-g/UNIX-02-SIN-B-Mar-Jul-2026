13854653562f67d48b6b4b5f4a26fba070c70d7f
sammy1208-g <samchamorroca@uide.edu.ec>
2026-06-17 13:07:00 +0000

    commit - newlogs...

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -19,3 +19,12 @@ grep "42.236.10.117" log.txt | awk '{print $7}'
 sed 's/Mozilla/Godzilla/g' log.txt
 
 sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+
+
+sed -i 's/Mozilla/Godzilla/g' log.txt
+
+sed 's/ //g' log.txt > newlog1.txt
+
+sed '1d' newlog.txt > newlogd.txt
+
+sed '$d' newlog.txt > newlogl.txt
